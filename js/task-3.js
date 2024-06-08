40a1f2f0fc3eb90de46423ed76c9986b1a8256a7
Anastasiia <stacey18work@gmail.com>
2024-06-08 13:25:51 +0300

    add js file

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth (content, padding, border) {
+    let res1 = Number.parseInt (content);
+    let res2 = Number.parseInt (padding);
+    let res3 = Number.parseFloat (border);
+    
+    return  res1 + (res2*2) + (res3*2);
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
